7cb19da110d06e1a5757eb89db9ad7aab62e3cef
Daniel Pauli <dapaulid@gmail.com>
2022-01-23 11:24:14 +0100

    Handle git submodules.

--- setup.sh
+++ setup.sh
@@ -3,20 +3,29 @@
 # exit on error
 set -e
 
-echo
-echo "================================================================================"
-echo "> building googletest"
-echo "================================================================================"
-cmake -S libs/googletest -B build/libs/googletest
-make -C build/libs/googletest
+# function for setting up a single submodule/library
+setup_submodule() {
+	modname=$1
+	cmake_args=${@:2}
+	echo
+	echo "================================================================================"
+	echo "> setting up $modname"
+	echo "================================================================================"
+	echo "[ git submodule ]"
+	git submodule update --init --recursive libs/$modname
+	git submodule status libs/$modname
+	echo "[ cmake ]"
+	cmake -S libs/$modname -B build/libs/$modname $cmake_args
+	echo "[ make ]"
+	make -C build/libs/$modname	
+	echo "[ done ]"
+}
 
-echo
-echo "================================================================================"
-echo "> building google benchmark"
-echo "================================================================================"
-cmake -S libs/benchmark -B build/libs/benchmark -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_GTEST_TESTS=OFF #-DGOOGLETEST_PATH=libs/googletest
-make -C build/libs/benchmark
+# do all submodules here
+setup_submodule "googletest"
+setup_submodule "benchmark" "-DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_TESTING=OFF"
 
+# done
 echo
 echo "================================================================================"
 echo "setup completed successfully!"
