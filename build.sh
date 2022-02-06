2bd84e74ce83d7abc55196ca5f8c82689e9a6d1d
Daniel Pauli <dapaulid@gmail.com>
2022-02-06 22:23:20 +0100

    Support for different platforms.

--- build.sh
+++ build.sh
@@ -1,5 +1,28 @@
 #!/bin/bash -e
 
-source libs/emsdk/emsdk_env.sh
-emcmake cmake -S . -B build
-emmake make -C build #VERBOSE=1
+platform=$1
+
+if [[ ! $platform ]]; then
+	echo "error: please specify platform: native or web"
+	exit 1
+fi
+
+case $platform in
+	"native")
+		cmake -S . -B build/$platform -DPLATFORM=$platform
+		cmake --build build/$platform
+		;;
+
+	"web")
+		source libs/emsdk/emsdk_env.sh
+		emcmake cmake -S . -B build/$platform -DPLATFORM=$platform
+		emmake make -C build/$platform #VERBOSE=1
+		;;
+
+	*)
+		echo "error: unsupported platform: $platform"
+		exit 1
+		;;
+esac
+
+
--- run.sh
+++ run.sh
@@ -1,20 +1,43 @@
 #!/usr/bin/env bash
 
 what=$1
+platform=$2
 
 if [[ ! $what ]]; then
 	echo "error: please specify what to run"
 	exit 1
 fi
 
-executable="build/${what%.*}.html"
-if [[ ! -f $executable ]]; then
-	echo "error: file not found: ${executable}"
+if [[ ! $platform ]]; then
+	echo "error: please specify platform: native or web"
 	exit 1
 fi
 
-source libs/emsdk/emsdk_env.sh
-emrun --browser=chrome \
-	--browser_info --hostname=localhost --kill_exit \
-	--browser_args="--headless --remote-debugging-port=0 --disable-gpu --disable-software-rasterizer" \
-	${executable}
+case $platform in
+	"native")
+		executable="build/$platform/${what%.*}"
+		if [[ ! -f $executable ]]; then
+			echo "error: file not found: ${executable}"
+			exit 1
+		fi	
+		./${executable}
+		;;
+
+	"web")
+		executable="build/$platform/${what%.*}.html"
+		if [[ ! -f $executable ]]; then
+			echo "error: file not found: ${executable}"
+			exit 1
+		fi	
+		source libs/emsdk/emsdk_env.sh 2> /dev/null
+		emrun --browser=chrome \
+			--browser_info --hostname=localhost --kill_exit \
+			--browser_args="--headless --remote-debugging-port=0 --disable-gpu --disable-software-rasterizer" \
+			${executable}
+		;;
+
+	*)
+		echo "error: unsupported platform: $platform"
+		exit 1
+		;;
+esac
