7469c49f31d33635c354b262330180b12d8b1f7c
Daniel Pauli <dapaulid@gmail.com>
2022-02-13 15:26:39 +0100

    More tests for command.

--- src/command.h
+++ src/command.h
@@ -1,88 +1,104 @@
-#include <stdio.h>
-#include <new>
 #include <cstddef> // std::max_align_t
 
-class BaseWrapper {
+template<int Size = 64>
+class Command final {
  public:
-    virtual ~BaseWrapper() {}
-	virtual void execute() = 0;
-    virtual void copy_to(void* a_dst) = 0;
-};
-
-template<typename F>
-class Wrapper final: public BaseWrapper {
- public:
-	explicit Wrapper(const F& a_func): m_func(a_func) {
-        printf("created function wrapper of size %lu\n", sizeof(*this));
-    }
-    ~Wrapper() {
-        printf("destroyed function wrapper of size %lu\n", sizeof(*this));
-    }
-	void execute() override {
-		m_func();
-	}
-    void copy_to(void* a_dst) override {
-        // placement new
-        ::new (a_dst) Wrapper(m_func);
-    }
- private:
-	F m_func;
-};
-
-class NullWrapper final: public BaseWrapper {
-	void execute() override {
-        // do nothing
-	}
-    void copy_to(void* a_dst) override {
-        // placement new
-        ::new (a_dst) NullWrapper();
-    }
-};
-
-class Command {
- public:
-    Command()
-        //m_wrapper(nullptr) {}
-        //m_wrapper(new (m_storage) Wrapper<decltype(nop)>(nop)) {}
-        //m_wrapper(new (m_storage) NullWrapper()) {}
-    {
-        new (m_memory) NullWrapper();
+    Command() {
+        init();
     }
         
 	template<typename F>
-	explicit Command(const F& a_func)
-        // placement new
-		//m_wrapper(new (m_storage) Wrapper<F>(a_func)) {}
-    {
-        static_assert(sizeof(Wrapper<F>) <= sizeof(m_memory), 
-            "function too big to store in command. did you use too many parameters?");
-        new (m_memory) Wrapper<F>(a_func);
+	Command(F&& a_func) {
+        init(std::move(a_func));
     }
+	
+
     // no copy constructor
     Command(const Command& other) = delete;
     // no copy assignment
     Command& operator=(const Command& other) = delete;
     // move assignment
     Command& operator=(Command&& other) noexcept {
-        //m_wrapper = other.m_wrapper;
+        reset();
         other.wrapper().copy_to(m_memory);
-        //other.m_wrapper = nullptr;
         return *this;
     }
+    // move assignment from callable
+    template<typename F>
+	Command& operator=(F&& a_func) {
+        reset();
+        init(std::move(a_func));
+        return *this;
+    }
+    // destructor    
 	~Command() {
-        // "placement delete"
-        //if (m_wrapper) {
-		    wrapper().~BaseWrapper();
-        //}
+        reset();
 	}
+
 	void execute() {
 		wrapper().execute();
 	}
  private:
+    class BaseWrapper;
     BaseWrapper& wrapper() {
         return reinterpret_cast<BaseWrapper&>(m_memory);
     }
+    template<typename F>
+    void init(F&& a_func) {
+        static_assert(sizeof(Wrapper<F>) <= sizeof(m_memory), 
+            "function too big to store in command. did you use too many parameters?");
+        // placement new
+        new (m_memory) Wrapper<F>(std::move(a_func));
+    }
+    void init(std::nullptr_t = nullptr) {
+        // placement
+        new (m_memory) NullWrapper();
+    }
+    void reset() {
+        // "placement delete"
+	    wrapper().~BaseWrapper();
+    }
+ // private helper classes
+ private: 
+
+    class BaseWrapper {
+    public:
+        virtual ~BaseWrapper() {}
+        virtual void execute() = 0;
+        virtual void move_to(void* a_dst) = 0;
+    };
+
+    template<typename F>
+    class Wrapper final: public BaseWrapper {
+    public:
+        explicit Wrapper(F&& a_func): m_func(std::move(a_func)) {
+            printf("created function wrapper of size %lu\n", sizeof(*this));
+        }
+        ~Wrapper() {
+            printf("destroyed function wrapper of size %lu\n", sizeof(*this));
+        }
+        void execute() override {
+            m_func();
+        }
+        void move_to(void* a_dst) override {
+            // placement new
+            ::new (a_dst) Wrapper(std::move(m_func));
+        }
+    private:
+        F m_func;
+    };
+
+    class NullWrapper final: public BaseWrapper {
+        void execute() override {
+            // do nothing
+        }
+        void move_to(void* a_dst) override {
+            // placement new
+            ::new (a_dst) NullWrapper();
+        }
+    };
+
+ // private members   
  private:
-    alignas(std::max_align_t) unsigned char m_memory [64];
-    //BaseWrapper* m_wrapper;
+    alignas(std::max_align_t) unsigned char m_memory [Size];
 };
--- test/command.test.cpp
+++ test/command.test.cpp
@@ -1,12 +1,32 @@
 #include <gtest/gtest.h>
 
 #include <queue>
+#include <functional>
 
 #include "command.h"
 #include "utils/copy_tracker.h"
 
+TEST(Command, Assign) {
+  CopyTracker t1, t2;
+
+  //std::function<void(void)> cmd;
+  Command cmd = [t1](){};
+  cmd = [t2](){};
+  cmd = nullptr;
+  
+  EXPECT_EQ(1, t1.get_copy_count());
+  EXPECT_EQ(1, t1.get_move_count());
+  EXPECT_EQ(2, t1.get_destroy_count());
+
+  EXPECT_EQ(1, t2.get_copy_count());
+  EXPECT_EQ(1, t2.get_move_count());
+  EXPECT_EQ(2, t2.get_destroy_count());  
+
+  //std::function f = [t](){};
+}
 
 TEST(Command, ExecLambda) {
+
   int foo = 1;
   CopyTracker tracker;
   Command cmd([&foo, tracker](){
@@ -17,13 +37,29 @@ TEST(Command, ExecLambda) {
   cmd.execute();
   EXPECT_EQ(2, foo);
   // non-functional: number of copies performed
-  // TODO: unclear why 2
-  EXPECT_EQ(2, tracker.get_copy_count());
+  EXPECT_EQ(1, tracker.get_copy_count());
+  EXPECT_EQ(1, tracker.get_move_count());
 }
+
+TEST(Command, ExecLambda2) {
+
+  int foo = 1;
+  CopyTracker tracker;
+  std::function cmd([&foo, tracker](){
+    foo++;
+  });
+
+  EXPECT_EQ(1, foo);
+  cmd();
+  EXPECT_EQ(2, foo);
+  // non-functional: number of copies performed
+  EXPECT_EQ(1, tracker.get_copy_count());
+}
+
 TEST(Command, ExecFromQueue) {
   int foo = 1;
 
-  std::queue<Command> q;
+  std::queue<Command<>> q;
   q.emplace([&foo](){
     foo++;
   });
